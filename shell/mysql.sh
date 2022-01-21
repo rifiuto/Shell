5ec2848441e3374df1cbea4686ac24c6d3502f8c
sayno <418215971@qq.com>
2022-01-21 20:36:01 +0800

    add a tk program

---
+++ shell/mysql.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# ********************************************************************************
+# Copyright © 2022 Rifiuto
+# File Name: mysql.sh
+# Author: Rifiuto
+# Email: sayno@Rifiuto
+# Created: 2022-01-17 16:21:15 (CST)
+# Last Update: 
+#          By: 
+# Description: 
+# ********************************************************************************
